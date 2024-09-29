2b021fcd18e3be821f4878c3fed8ae41af07eac7
Nhn2025 <dnhu122025@gmail.com>
2024-09-30 01:25:02 +0700

    fix error testNG

--- testcases/com/nopcommerce/account/Account_01_Register.java
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -1,15 +1,23 @@
 package com.nopcommerce.account;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Account_01_Register {
-    @BeforeTest
-    public void beforeClass() {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
 
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
     }
-    //
 
     @Test
     public void Register_01_Empty_Data() {
