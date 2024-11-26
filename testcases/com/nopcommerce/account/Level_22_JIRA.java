d9b2031c66989ee40cb3c96a8d2c5f6b0a8acd2b
Nhn2025 <dnhu122025@gmail.com>
2024-11-26 09:24:03 +0700

    Intergrate Selenium with JIRA

--- testcases/com/nopcommerce/account/Level_22_JIRA.java
+++ testcases/com/nopcommerce/account/Level_22_JIRA.java
@@ -33,7 +33,6 @@ public class Level_22_JIRA extends BaseTest {
         homePage = PageGeneratorManager.getHomePage(driver);
     }
 
-    //
     @JiraCreateIssueService.JiraCreateIssue(isCreateIssue = true)
     @Test
     public void User_01_Register_Success() {
